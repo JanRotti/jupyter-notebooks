14212290c4beb3b99cbdf3559bec8b81221b90f7
Jan Rottmayer <62800131+JanRotti@users.noreply.github.com>
2020-10-19 18:02:47 +0200

    Create Hyperparameters_config.py

---
+++ GAN/Hyperparameters_config.py
@@ -0,0 +1,11 @@
+HyperparameterDefaults = {
+    "Dropout" : 0.3,
+    "GDimension" : 7,
+    "BatchSize" : 128,
+    "LearningRate" : 0.001,
+    "Epochs" : 50,
+    "LatentDim" : 128,
+    "GenFMaps" : 256,
+    "DisFMaps" : 64,
+    "SampleInterval" : 20,
+}
